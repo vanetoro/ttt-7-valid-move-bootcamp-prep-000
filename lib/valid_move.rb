71f718ff400d2c3b2ba1d7dadac1bcb51299d9de
Vanessa Toro <vanetoro84@gmail.com>
2018-03-11 18:41:35 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 def valid_move?(board, index)
  if board[index] == ' ' || board[index] == '' || board[index] == nil
    return true
-  else if board[index] < 0 || board[index] > 9
+  elsif board[index] < 0 || board[index] > 9
     return false 
   else 
     return false
