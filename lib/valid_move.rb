39445e300318d7bed06ca779aeef97de2be0a66a
Vanessa Toro <vanetoro84@gmail.com>
2018-03-11 18:33:12 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,9 @@
 # code your #valid_move? method here
-def valid_move?
-  
+def valid_move?(move)
+  if move > 0 && move <= 9
+    return true
+  else
+    return false
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
